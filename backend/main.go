105030e67d97dec96f19743f11451b8aec4f4b84
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-11-19 12:28:17 +0000

    🚧 Refactor estructure

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/fullstacktf/ControlHorarios-Backend/tree/estructura_go/backend/api/config"
+	s "github.com/fullstacktf/ControlHorarios-Backend/tree/estructura_go/backend/api/infraestructure"
+	"github.com/fullstacktf/ControlHorarios-Backend/tree/estructura_go/backend/api/model"
 	"github.com/fullstacktf/ControlHorarios-Backend/tree/estructura_go/backend/api/routes"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
+var err error
+
 func main() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Silent,
+			Colorful:      true,
+		},
+	)
+	s.DB, err = gorm.Open(mysql.Open(config.DbURL(config.BuildDBConfig())), &gorm.Config{Logger: newLogger})
+	if err != nil {
+		log.Fatal("error al conectar a la base de datos:", err)
+	}
+
+	s.DB.AutoMigrate(&model.TipoCoche{}, &model.Coche{}, &model.Cliente{}, &model.Alquiler{})
+
 	routes.SetupRouter().Run(":8080")
 }
