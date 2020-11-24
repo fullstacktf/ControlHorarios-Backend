9115bdefb9acbd0595b07142a12a65bda9ef29a6
AiranSchez <airanschez0@gmail.com>
2020-11-24 11:53:38 +0000

    :recycle: Refactored routes

--- backend/api/routes/routes.go
+++ backend/api/routes/routes.go
@@ -14,5 +14,36 @@ func SetupRouter() *gin.Engine {
 		v1.PUT("/users/:id", controllers.UpdateUser)
 		v1.DELETE("/users/:id", controllers.DeleteUser)
 	}
+
+	employees := r.Group("/api/employee"){
+		employees.GET("/:id")
+		employees.GET("/:id/summary") 
+
+		employees.POST("/")
+		employees.POST("/login")
+		employees.POST("/:id/checkin") 
+
+		employees.PUT("/:id/password") 
+		employees.PUT("/:id/checkout") 
+	}
+
+	companies:= r.Group("/api/companies"){
+		companies.GET("/:id/holidays")
+		companies.GET("/:id/employees")
+		companies.GET("/:id/projects") 
+		companies.GET("/:id/sections")
+
+		companies.POST("/")
+		companies.POST("/login")
+		companies.POST("/:id/holidays")
+		companies.POST("/:id/projects") 
+		companies.POST("/:id/sections")
+
+		companies.PUT("/:id/projects")
+		companies.PUT("/:id/sections")
+		companies.PUT("/:id/holidays")
+				
+		companies.DELETE("/:id/holidays")
+	}
 	return r
 }
