99fbeb60b437aa87a11d6f7a3ac07cca29bf8313
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-11-26 13:42:00 +0000

    :sparkles: Feature added test suite

---
+++ backend/tests/company_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func CreateCompanyShouldReturn200(t *testing.T) {
+
+	assert.NotEqual(t, 1, 2, "No son iguales")
+}
