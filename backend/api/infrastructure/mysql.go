37125ea0581ddad91518ac732eb72bd2fbe81dfb
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-11-29 12:37:28 +0000

    🚧 Added singleton pattern to database connection

--- backend/api/infrastructure/mysql.go
+++ backend/api/infrastructure/mysql.go
@@ -14,6 +14,8 @@ type Tabler interface {
 	TableName() string
 }
 
+var database *gorm.DB
+
 func DB() *gorm.DB {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags),
@@ -23,9 +25,14 @@ func DB() *gorm.DB {
 			Colorful:      true,
 		},
 	)
-	DB, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/controlhorarios?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{Logger: newLogger})
-	if err != nil {
-		log.Fatal("error al conectar a la base de datos:", err)
+
+	if database == nil {
+		var err error
+		database, err = gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/controlhorarios?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{Logger: newLogger})
+		if err != nil {
+			log.Fatal("error al conectar a la base de datos:", err)
+		}
 	}
-	return DB
+
+	return database
 }
